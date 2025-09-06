4f8b72f82c67b5ebd748fdf2c5162bf1c31a2822
As0581 <zagirovaslan1@gmail.com>
2025-09-06 15:22:44 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".mySwiper", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      initialSlide: 1,
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 1000,
+        modifier: 1,
+        slideShadows: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
